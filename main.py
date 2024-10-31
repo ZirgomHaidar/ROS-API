62ad76a5bcfa98a36a8609c792d82a0951c0c484
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-10-31 17:08:52 +0530

    add a get route(/device/{codename}) to grab device data

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI(title="ROSAPI", version="0.1.0")
 
+# API entry
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"API_Status" :"The API is Up and Running!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/device/{codename}")
+async def get_device(codename: str):
+     variants_data : list = []
+
+     vanilla_response = requests.get(f"https://raw.githubusercontent.com/ZirgomHaidar/android_vendor_RisingOTA/refs/heads/fourteen/VANILLA_{codename}.json")
+     core_response = requests.get(f"https://raw.githubusercontent.com/ZirgomHaidar/android_vendor_RisingOTA/refs/heads/fourteen/CORE_{codename}.json")
+     gapps_response = requests.get(f"https://raw.githubusercontent.com/ZirgomHaidar/android_vendor_RisingOTA/refs/heads/fourteen/GAPPS_{codename}.json")
+
+     if vanilla_response.status_code == 200:
+        vanilla_data = vanilla_response.json()
+        variants_data.append(vanilla_data['response'][0])
+
+     if core_response.status_code == 200:
+        core_data = core_response.json()
+        variants_data.append(core_data['response'][0])
+
+     if gapps_response.status_code == 200:
+        gapps_data = gapps_response.json()
+        variants_data.append(gapps_data['response'][0])
+
+     return variants_data # the data will always return in order vanilla -> core -> gapps
